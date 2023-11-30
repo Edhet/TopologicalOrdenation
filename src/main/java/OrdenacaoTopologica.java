5dcf435edd064144e6e1fdb8db4b78597fc5dbed
Lucca Braga <133079954+luccabcarvalho@users.noreply.github.com>
2023-11-30 11:25:38 -0300

    Topological Sort test methods added

--- src/main/java/OrdenacaoTopologica.java
+++ src/main/java/OrdenacaoTopologica.java
@@ -4,13 +4,13 @@ public class OrdenacaoTopologica
 
 	public static class Elo
 	{
-		/* Identificação do elemento. */
+		/* IdentificaÃ§Ã£o do elemento. */
 		public int chave;
 		
-		/* Número de predecessores. */
+		/* NÃºmero de predecessores. */
 		public int contador;
 		
-		/* Aponta para o próximo elo da lista. */
+		/* Aponta para o prÃ³ximo elo da lista. */
 		public Elo prox;
 		
 		/* Aponta para o primeiro elemento da lista de sucessores. */
@@ -40,10 +40,10 @@ public class OrdenacaoTopologica
 	
 	public static class EloSuc
 	{
-		/* Aponta para o elo que é sucessor. */
+		/* Aponta para o elo que Ã© sucessor. */
 		public Elo id;
 		
-		/* Aponta para o próximo elemento. */
+		/* Aponta para o prÃ³ximo elemento. */
 		public EloSuc prox;
 		
 		public EloSuc()
@@ -60,10 +60,10 @@ public class OrdenacaoTopologica
 	}
 
 
-	/* Ponteiro (referência) para primeiro elemento da lista. */
+	/* Ponteiro (referÃªncia) para primeiro elemento da lista. */
 	public Elo prim;
 	
-	/* Número de elementos na lista. */
+	/* NÃºmero de elementos na lista. */
 	private int n;
 		
 	public OrdenacaoTopologica()
@@ -72,23 +72,82 @@ public class OrdenacaoTopologica
 		n = 0;
 	}
 	
-	/* Método responsável pela leitura do arquivo de entrada. */
+	/* MÃ©todo responsÃ¡vel pela leitura do arquivo de entrada. */
 	public void realizaLeitura(String nomeEntrada)
 	{
 		/* Preencher. */
 	}
 	
-	/* Método para impressão do estado atual da estrutura de dados. */
+	/* MÃ©todo para impressÃ£o do estado atual da estrutura de dados. */
 	private void debug()
 	{
 		/* Preencher. */
 	}
 	
-	/* Método responsável por executar o algoritmo. */
+	/* MÃ©todo responsÃ¡vel por executar o algoritmo. */
 	public boolean executa()
 	{
 		/* Preencher. */
 		
 		return false;
+	}
+
+	public Elo smallerEloSearch()
+	{
+		Elo p;
+		Elo q = null;
+
+		p = prim;
+		prim = null;
+
+		while(p != null)
+		{
+			q = p;
+			p = q.prox;
+			if(q.contador == 0)
+			{
+				q.prox = prim;
+				prim = q;
+			}
+		}
+		return q;
+	}
+
+	public void topologicalSort()
+	{
+		Elo q;
+		EloSuc aux;
+
+		while(n > 0)
+		{
+			q = smallerEloSearch();
+
+			System.out.println(q.chave);
+			n--;
+			q.listaSuc.id.contador--;
+			if (q.listaSuc.id.contador == 0)
+			{
+				q.prox = q.listaSuc.id;
+				q.listaSuc.id = q.listaSuc.prox.id;
+			}
+
+			aux = q.listaSuc.prox;
+			while(aux.prox != null) {
+				aux.id.contador--;
+				if (aux.id.contador == 0)
+					q.prox = aux.id;
+				aux.prox = aux.prox.prox;
+
+				aux = aux.prox;
+			}
+
+		}
+	}
+
+	public void print()
+	{
+		Elo p;
+		for(p = prim; p != null; p = p.prox)
+			System.out.println(p.chave);
 	}
 }
